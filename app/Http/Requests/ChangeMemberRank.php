5ca432380da7ecba4436b1694e025a17d3f00280
Chris Deaton <dcdeaton@ncsu.edu>
2021-01-02 10:59:20 -0500

    update policy

--- app/Http/Requests/ChangeMemberRank.php
+++ app/Http/Requests/ChangeMemberRank.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Http\FormRequest;
 class ChangeMemberRank extends FormRequest
 {
     /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('update', $this->route()->member);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
