43df927c8dc12ccb7a652bad4c298e6f965da33e
Chris Deaton <dcdeaton@ncsu.edu>
2021-01-02 10:49:49 -0500

    enforce policy authorization on controller

--- app/Http/Requests/ChangeMemberRank.php
+++ app/Http/Requests/ChangeMemberRank.php
@@ -7,16 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class ChangeMemberRank extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return $this->user()->can('update', $this->member);
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
