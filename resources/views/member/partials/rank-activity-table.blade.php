a61101da67abe56169e875318885455bb9bd8947
Chris Deaton <dcdeaton@ncsu.edu>
2021-01-18 19:58:28 -0500

    move rank update to modal

--- resources/views/member/partials/rank-activity-table.blade.php
+++ resources/views/member/partials/rank-activity-table.blade.php
@@ -4,7 +4,7 @@
         <tr>
             <th>Rank</th>
             <th>Date</th>
-            <th>Updated By</th>
+            <th>Admin</th>
         </tr>
         </thead>
         @foreach ($rankHistory as $rank)
@@ -18,3 +18,12 @@
 @else
     <p><span class="text-muted">No rank activity</span></p>
 @endif
+
+
+@can('update', $member)
+    <hr>
+    <p>
+        <a href="#" class="btn-add-note btn btn-default" data-toggle="modal"
+           data-target="#rank-form">Update Rank</a>
+    </p>
+@endcan
--- resources/views/member/rank/edit.blade.php
+++ resources/views/member/rank/edit.blade.php
@@ -29,43 +29,7 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="panel panel-filled">
-                    <form action="{{ route('member.rank.update', $member->getUrlParams()) }}" method="post">
-                        {{ csrf_field() }}
-                        <div class="panel-heading panel-b-accent">
-                            <strong class="c-white">Rank</strong>
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="rank">New rank</label>
-                                        <select name="rank" id="rank" class="form-control">
-                                            @foreach ($ranks as $rank)
-                                                <option value="{{ $rank->id }}">{{ $rank->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    {!! Form::label('created_at', 'Effective date') !!} <span
-                                        class="text-accent">*</span>
-                                    {{ Form::date('created_at', now(), ['class' => 'form-control', 'required' => 'required']) }}
-                                </div>
-                            </div>
 
-                            <div class="form-group">
-                                <input type="checkbox" name="historical" id="historical">
-                                <label for="historical">Historical entry?</label>
-                                <p class="help-text">Enabling this will leave the member's current rank intact.</p>
-                            </div>
-
-                        </div>
-
-                        <div class="panel-footer">
-                            <button class="btn btn-success" type="submit">Submit</button>
-                        </div>
-
-                    </form>
 
 
                 </div>
